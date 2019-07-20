dc711b645547c605dca5ea739b3d360507e8f560
Kevin Pheasey <kevin@kpsoftware.io>
2019-07-19 20:56:42 -0400

    fix(selectors): getRelationship returns null when relationship is undefined

--- src/selectors.js
+++ src/selectors.js
@@ -5,7 +5,7 @@ const findRecord = (records, id) => records[id] || records[String(id)] || null;
 export const getRecord = (api, { type, id }) => findRecord(getType(api, { type }), id);
 
 export const getRelationship = (api, relationship) => {
-  if (!relationship.data) return null;
+  if (!relationship || !relationship.data) return null;
 
   if (Array.isArray(relationship.data)) {
     return relationship.data
@@ -13,6 +13,5 @@ export const getRelationship = (api, relationship) => {
       .filter(rel => rel);
   }
 
-  const included = api[relationship.data.type] || {};
-  return included[relationship.data.id] || null;
+  return getRecord(api, relationship.data);
 };
