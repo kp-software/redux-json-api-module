993b3012bb4090b7e2c4bb634156fab449d5d709
Kevin Pheasey <kevin.pheasey@gmail.com>
2025-02-12 17:06:40 -0500

    iron out all IDE warnings

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,35 @@
 import { Action } from 'redux';
 
-declare module 'redux-json-api-module' {
-    export type ApiRecord = {
-        id?: string | number;
-        type: string;
-        attributes: any;
-    };
+export type ApiRecord = {
+    id?: string | number;
+    type: string;
+    attributes: any;
+};
 
-    export type Relationship = {
-        data: ApiRecord | Array<ApiRecord>;
-    };
-
-    export const CLEAR_RECORDS: string;
-    export const FETCH_RECORDS: string;
-    export const FETCH_RECORDS_SUCCESS: string;
-    export const SAVE_RECORD: string;
-    export const SAVE_RECORD_SUCCESS: string;
-    export const DELETE_RECORD: string;
-    export const DELETE_RECORD_FAIL: string;
-    export const clearRecords: (type: string) => void;
-    export const getRecord: (state: any, { type, id }: { type: string; id: number }) => any;
-    export const fetchRecords: (type: string, params?: object) => Action;
-    export const fetchRecord: (type: string , id: any, params?: object) => Action;
-    export const saveRecord: (record: ApiRecord, params?: object) => Action;
-    export const deleteRecord: (record: ApiRecord) => Action;
-    export const getRelationship: (state: any, relationship: any) => any;
-    export const queryString: (params: any) => string;
+export type RecordSet = {
+    [key: string]: ApiRecord;
 }
+
+export type ApiStore = {
+    [key: string]: RecordSet;
+}
+
+export type Relationship = {
+    data: ApiRecord | Array<ApiRecord>;
+};
+
+export const CLEAR_RECORDS: string;
+export const FETCH_RECORDS: string;
+export const FETCH_RECORDS_SUCCESS: string;
+export const SAVE_RECORD: string;
+export const SAVE_RECORD_SUCCESS: string;
+export const DELETE_RECORD: string;
+export const DELETE_RECORD_FAIL: string;
+export const clearRecords: (type: string) => void;
+export const getRecord: (state: any, { type, id }: { type: string; id: number }) => any;
+export const fetchRecords: (type: string, params?: object) => Action;
+export const fetchRecord: (type: string , id: any, params?: object) => Action;
+export const saveRecord: (record: ApiRecord, params?: object) => Action;
+export const deleteRecord: (record: ApiRecord) => Action;
+export const getRelationship: (state: any, relationship: any) => any;
+export const queryString: (params: any) => string;
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,21 +1,25 @@
-import type { ApiRecord, Relationship } from '../index.d';
+import type { ApiStore, ApiRecord, RecordSet, Relationship } from '../index.d';
 
-const emptyRecords: ApiRecord<string, any> = {};
+const emptyRecords: RecordSet = {};
 
-const getType = (api: ApiRecord<string, any>, { type }: { type: string }) => api[type] || emptyRecords;
+const getType = (api: ApiStore, { type }: { type: string }) => api[type] || emptyRecords;
 
-const findRecord = (records: ApiRecord<string, any>, id: string | number) => records[id] || records[String(id)] || null;
+const findRecord = (records: RecordSet, id: string | number) => records[id] || records[String(id)] || null;
 
-export const getRecord = (api: ApiRecord<string, any>, { type, id }: { type: string; id: string | number }) => findRecord(getType(api, { type }), id);
+export const getRecord = (api: ApiStore, { type, id }: { type: string; id: string | number }) => findRecord(getType(api, { type }), id);
 
-export const getRelationship = (api: ApiRecord<string, any>, relationship: Relationship) => {
+export const getRelationship = (api: ApiStore, relationship: Relationship) => {
   if (!relationship || !relationship.data) return null;
 
   if (Array.isArray(relationship.data)) {
     return relationship.data
-        .map(rel => rel.id !== undefined ? getRecord(api, rel) : null)
-        .filter(rel => rel);
+        .map((rel: ApiRecord) => rel.id !== undefined ? getRecord(api, { type: rel.type, id: rel.id }) : null)
+        .filter((rel: ApiRecord | null) => rel);
   }
 
-  return relationship.data.id !== undefined ? getRecord(api, relationship.data) : null;
+  if (relationship.data.id !== undefined){
+     return getRecord(api, { type: relationship.data.type, id: relationship.data.id })
+  }
+
+  return null
 };
